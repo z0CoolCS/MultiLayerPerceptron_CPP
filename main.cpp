1f92bcb03eb5f4d323aa5deba22ea47891805628
jorge rebosio <jorge.rebosio@utec.edu.pe>
2021-07-08 17:08:45 -0500

    update main

--- main.cpp
+++ main.cpp
@@ -7,6 +7,131 @@
 using namespace std;
 using namespace Eigen;
 
+
+
+//void Sigmoid1(MatrixXd& X, MatrixXd& Y)
+//{
+	
+//}
+
+
+void Tanh1(MatrixXd& X, MatrixXd& Y)
+{
+	MLP ne;
+    ne.add_layer( "Linear" , X.cols() , 15 , 0.00001);
+    ne.add_layer( "tanh" );
+    ne.add_layer( "linear" , 15 , 2, 0.00001); 
+    
+	
+	ne.train(X, Y) ;		
+}
+
+
+void Tanh2(MatrixXd& X, MatrixXd& Y)
+{
+	MLP ne;
+    ne.add_layer( "Linear" , X.cols() , 15 , 0.00001);
+    ne.add_layer( "tanh" );
+    ne.add_layer( "linear" , 15 , 2, 0.00001);
+    ne.add_layer( "tanh" ); 
+    //ne.add_layer( "linear" , 15 , 2, 0.00001);	
+	
+	ne.train( X, Y) ;		
+}
+
+
+void Tanh3(MatrixXd& X, MatrixXd& Y)
+{
+	MLP ne;
+    ne.add_layer( "Linear" , X.cols() , 15 , 0.00001);
+    ne.add_layer( "tanh" );
+    ne.add_layer( "linear" , 15 , 15, 0.00001);
+    ne.add_layer( "tanh" ); 
+    ne.add_layer( "linear" , 15 , 2, 0.00001);
+    ne.add_layer( "tanh" );
+    
+    ne.train( X, Y) ;
+	
+}
+
+void Sigmoid1(MatrixXd& X, MatrixXd& Y)
+{
+	MLP ne;
+    ne.add_layer( "Linear" , X.cols() , 15 , 0.00001);
+    ne.add_layer( "sigmoid" );
+    ne.add_layer( "linear" , 15 , 2, 0.00001); 
+    
+	
+	ne.train(X, Y) ;		
+}
+
+
+void Sigmoid2(MatrixXd& X, MatrixXd& Y)
+{
+	MLP ne;
+    ne.add_layer( "Linear" , X.cols() , 15 , 0.00001);
+    ne.add_layer( "sigmoid" );
+    ne.add_layer( "linear" , 15 , 2, 0.00001);
+    ne.add_layer( "sigmoid" ); 
+    //ne.add_layer( "linear" , 15 , 2, 0.00001);	
+	
+	ne.train( X, Y) ;		
+}
+
+
+void Sigmoid3(MatrixXd& X, MatrixXd& Y)
+{
+	MLP ne;
+    ne.add_layer( "Linear" , X.cols() , 15 , 0.00001);
+    ne.add_layer( "sigmoid" );
+    ne.add_layer( "linear" , 15 , 15, 0.00001);
+    ne.add_layer( "sigmoid" ); 
+    ne.add_layer( "linear" , 15 , 2, 0.00001);
+    
+    ne.train( X, Y) ;
+	
+}
+
+void Relu1(MatrixXd& X, MatrixXd& Y)
+{
+	MLP ne;
+    ne.add_layer( "Linear" , X.cols() , 15 , 0.00001);
+    ne.add_layer( "relu" );
+    ne.add_layer( "linear" , 15 , 2, 0.00001); 
+    ne.add_layer( "relu" ); 
+	
+	ne.train(X, Y) ;		
+}
+
+
+void Relu2(MatrixXd& X, MatrixXd& Y)
+{
+	MLP ne;
+    ne.add_layer( "Linear" , X.cols() , 15 , 0.00001);
+    ne.add_layer( "relu" );
+    ne.add_layer( "linear" , 15 , 15, 0.00001);
+    ne.add_layer( "relu" ); 
+    ne.add_layer( "linear" , 15 , 2, 0.00001);	
+	
+	ne.train( X, Y) ;		
+}
+
+
+void Relu3(MatrixXd& X, MatrixXd& Y)
+{
+	MLP ne;
+    ne.add_layer( "Linear" , X.cols() , 15 , 0.00001);
+    ne.add_layer( "relu" );
+    ne.add_layer( "linear" , 15 , 15, 0.00001);
+    ne.add_layer( "relu" ); 
+    ne.add_layer( "linear" , 15 , 2, 0.00001);
+    ne.add_layer( "relu" ); 
+    
+    ne.train( X, Y) ;
+	
+}
+
+
 int main () {
     
     string path = "Carncer DataSet - Hoja 1.csv";
@@ -16,32 +141,30 @@ int main () {
 
     MatrixXd data = read_csv(rowsdata, colsdata, path);
     
-
     
     
-    //MatrixXd data = data2(seq(1, 100), seq(0, 4));
     
     MatrixXd x = data( all , seq(1, last)); // caracteristicas
-	 
+     
 	MatrixXd y = data( all , 0); // clases , columna 0
 	
-    //cout << x;
-    
-    //cout << x << endl;
-    //cout << y << endl;
-    
-    // 0.5 0.6
-    MLP a;
-    a.add_layer( "Linear" , x.cols() , 15 , 0.00001);
-    a.add_layer( "tanh" );
-    a.add_layer( "linear" , 15 , 2, 0.00001); 
-
-	a.train(x, y) ;
-    //MatrixXd tmp(2, 3);
-    //tmp << 0, 0, -1, 1,1,1;
-    //cout << tmp << endl;
-    //ReLU d;
-    //cout<<d.backward(tmp, tmp);
-
+	// Tanh
+	
+	Tanh1(x, y);
+	//Tanh2(x, y);
+	//Tanh3(x, y);
+	
+	// Sigmoid
+	
+	//Sigmoid1(x, y);
+	//Sigmoid2(x, y);
+	//Sigmoid3(x, y);
+	
+	
+	// relu
+	
+	//Relu1(x, y);
+	//Relu2(x, y);
+	//Relu3(x, y);
     return 0;
 }
